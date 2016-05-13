25a897599dbb2d99f4b3b26b1ea5c04fc0c9177b
Miguel Costero <miguelcostero@outlook.com>
2016-05-13 14:55:42 -0400

    usando nodejs v6.0.0 y utilizando express

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var app = express();
+
+//IP  y puerto de la app
+var ipaddress, port;
+ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP;
+port      = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+
+if (typeof ipaddress === "undefined") {
+    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+    //  allows us to run/test the app locally.
+    console.warn('No OPENSHIFT_*_IP var, using 127.0.0.1');
+    ipaddress = "127.0.0.1";
+};
+
+//Archivos estaticos
+app.use('/public', express.static('public'));
+app.use('/public', express.static('bower_components'));
+
+//utilizacion del body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//rutas
+app.get('/', function (req, res) {
+	document.write('Hola Mundo!');
+});
+
+app.post('/users', function (req, res) {
+	console.log("Email: "+req.body.email);
+	console.log("Contraseña: "+req.body.pass);
+	res.send('Hemos recibido tus datos.');
+});
+
+//ejecucion del server
+app.listen(port, ipaddress, function () {
+	console.log('%s: Node server started on %s:%d ...', Date(Date.now() ), ipaddress, port);
+});

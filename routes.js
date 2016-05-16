c6d188ce2677ca05ec1ba442456be32a172e6f62
Miguel Costero <miguelcostero@outlook.com>
2016-05-16 12:51:07 -0400

    agregando get de peliculas

--- routes.js
+++ routes.js
@@ -15,6 +15,7 @@ router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
+
 // define the home page route
 router.get('/', function(req, res) {
   res.send('API home page');
@@ -24,7 +25,7 @@ router.get('/empleados', function (req, res) {
   con.query('SELECT * FROM Empleado', function(err, rows) {
     if(err) throw err;
 
-    res.setHeader('Content-Type', 'routerlication/json');
+    res.setHeader('Content-Type', 'application/json');
     res.send(rows);
   });
 });
@@ -33,7 +34,17 @@ router.get('/peliculas', function (req, res) {
   con.query('SELECT * FROM Pelicula', function(err, rows) {
     if(err) throw err;
 
-    res.setHeader('Content-Type', 'routerlication/json');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(rows);
+  });
+});
+
+router.get('/peliculas/:id_pelicula', function (req, res) {
+  var id_pelicula = req.params.id_pelicula;
+  con.query('SELECT * FROM Pelicula WHERE Pelicula.idPelicula = " ' + id_pelicula + ' "', function(err, rows) {
+    if(err) throw err;
+
+    res.setHeader('Content-Type', 'application/json');
     res.send(rows);
   });
 });
